faf721cadca28515f5a22f90b18e033bf92f71e8
FreshDev <billyhans90@gmail.com>
2022-07-31 14:43:08 +0100

    tuples

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-let numbers: number[] = [];
-numbers[0] = 1;
-numbers[1] = 2;
+let user: [number, string] = [1, 'Hans'];
+user.push(1);
+//Tuples are useful when you only have key value pairs and always endeavour to keep it at two values.
