d56a4c264e2c279f07b5734822de771d6a4e0411
FreshDev <billyhans90@gmail.com>
2022-08-01 09:59:07 +0100

    type assertions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-let speed: number | null = null;
-let ride = {
-    speed: speed ?? 30,
-}
+let phone = <HTMLInputElement> document.getElementById('phone');
+// HTMLElement
+// HTMLInputElement
+phone.value;
