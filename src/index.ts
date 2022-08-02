c0063f05087963a2e3e891a2c659132a5f320dd6
FreshDev <billyhans90@gmail.com>
2022-08-02 16:53:35 +0100

    Access control key word

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
 class Account {
     readonly id: number;
     owner: string;
-    balance: number;
+    private _balance: number;
     nickname?: string;
 
     constructor(id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
 
     deposit(amount: number): void {
         if (amount <= 0)
             throw new Error('Invalid amount');
-        this.balance += amount;
+        this._balance += amount;
 
+    }
+
+private calculateTax() {
+
+}
+
+    getBalance(): number {
+        return account._balance;
     }
 }
 
 let account = new Account(1, 'Hans', 0);
+console.log(account.getBalance());
+
