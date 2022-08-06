8fbb98190c9dd9a63bb3c22636255a437bfe992f
FreshDev <billyhans90@gmail.com>
2022-08-06 13:39:26 +0100

    generic functions

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,9 @@
-class Person {
-    constructor (public firstName: string, public lastName: string){}
-
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`;
-    }
+class ArrayUtils {
+wrapInArray<T>(value: T) {
+return [value];
 }
 
-class Employee extends Person {
-    constructor (
-        firstName: string,
-        lastName: string,
-        public salary: number,
-    ) {
-        super(firstName, lastName);
-    }
 }
 
-interface Address {
-    street: string;
-    city: string;
-    zipCode: number;
-}
-
-interface Employee {
-    name: string;
-    salary: number;
-    address: Address;
-}
+let utils = new ArrayUtils();
+let numbers = utils.wrapInArray('1');
