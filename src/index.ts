c4a9320ec250555b87d6e90344f962bbec3d67e3
FreshDev <billyhans90@gmail.com>
2022-08-10 08:24:03 +0100

    Parameterized Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-function Component(constructor: Function) {
-    console.log('Component decorator called');
-    constructor.prototype.uniqueId = Date.now();
-    constructor.prototype.insertInDom = () => {
-        console.log('Inserting the component in DOM');
+type ComponentOptions = {
+    selector: string;
+}
+
+//Decorator factory
+function Component(options:ComponentOptions) {
+    
+    return  (constructor: Function) => {
+        console.log('Component decorator called');
+        constructor.prototype.options = options;
+        constructor.prototype.uniqueId = Date.now();
+        constructor.prototype.insertInDom = () => {
+            console.log('Inserting the component in DOM');
+        }
     }
 }
 
-@Component
+
+@Component({selector: '#my-profile'})
 class ProfileComponent{
 
 }
